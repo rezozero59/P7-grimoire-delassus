027cc40073464b05dc17ec6a3c86ce500d2fe2f3
rezozero59 <laurentdelassus@gmail.com>
2023-10-23 15:33:32 +0200

    test repo

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my app!" });
+});
+
+
+
+module.exports = app;
