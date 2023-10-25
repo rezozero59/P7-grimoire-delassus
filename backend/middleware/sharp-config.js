f00a8b39faca11bd99494308b5ca1e09c367826a
rezozero59 <laurentdelassus@gmail.com>
2023-10-25 11:15:45 +0200

    Optimisation des images avec sharp

---
+++ backend/middleware/sharp-config.js
@@ -0,0 +1,27 @@
+const sharp = require("sharp");
+const fs = require("fs");
+
+const sharpMiddleware = (req, res, next) => {
+  // Vérifiez si un fichier a été téléchargé
+  if (!req.file) {
+    return next(); // Si aucun fichier, passez au middleware/route suivant(e)
+  }
+
+  const filePath = req.file.path;
+
+  sharp(filePath)
+    .resize(800, 600) // Exemple de redimensionnement
+    .toBuffer()
+    .then((data) => {
+      // Réécriture de l'image originale par l'image traitée
+      fs.writeFile(filePath, data, (err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        next();
+      });
+    })
+    .catch((err) => res.status(500).send(err));
+};
+
+module.exports = sharpMiddleware;
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bookController = require("../controllers/book");
 const auth = require("../middleware/authToken");
 const multer = require("../middleware/multer-config");
+const sharpMiddleware = require("../middleware/sharp-config");
+
 const router = express.Router();
 
-router.post("/", auth, multer, bookController.createBook);
+router.post("/", auth, multer, sharpMiddleware, bookController.createBook);
 
-router.put("/:id", auth, multer, bookController.updateBook);
+router.put("/:id", auth, multer, sharpMiddleware, bookController.updateBook);
 
 router.post("/:id/rating", auth, bookController.createRating);
 
